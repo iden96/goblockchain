e8f0c552e5521083d6f322790b58540f8ca6d920
Denis Krugly <krugly_d@coinmatics.io>
2022-06-28 10:42:10 +0300

    [feature] implement blockchain wallet

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"goblockchain/internal/domain/blockchain"
+	"goblockchain/internal/domain/wallet"
 	"log"
 )
 
@@ -11,20 +11,25 @@ func init() {
 }
 
 func main() {
-	myBlockchainAddress := "my_blockchain_address"
-	blockchain := blockchain.NewBlockchain(myBlockchainAddress)
-	blockchain.Print()
+	// myBlockchainAddress := "my_blockchain_address"
+	// blockchain := blockchain.NewBlockchain(myBlockchainAddress)
+	// blockchain.Print()
 
-	blockchain.AddTransaction("A", "B", 1.0)
-	blockchain.Mining()
-	blockchain.Print()
+	// blockchain.AddTransaction("A", "B", 1.0)
+	// blockchain.Mining()
+	// blockchain.Print()
 
-	blockchain.AddTransaction("C", "D", 2.0)
-	blockchain.AddTransaction("X", "Y", 3.0)
-	blockchain.Mining()
-	blockchain.Print()
+	// blockchain.AddTransaction("C", "D", 2.0)
+	// blockchain.AddTransaction("X", "Y", 3.0)
+	// blockchain.Mining()
+	// blockchain.Print()
 
-	fmt.Printf("my %.1f\n", blockchain.CalculateTotalAmount("my_blockchain_address"))
-	fmt.Printf("D %.1f\n", blockchain.CalculateTotalAmount("D"))
-	fmt.Printf("C %.1f\n", blockchain.CalculateTotalAmount("C"))
+	// fmt.Printf("my %.1f\n", blockchain.CalculateTotalAmount("my_blockchain_address"))
+	// fmt.Printf("D %.1f\n", blockchain.CalculateTotalAmount("D"))
+	// fmt.Printf("C %.1f\n", blockchain.CalculateTotalAmount("C"))
+
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
+	fmt.Println(w.BlockchainAddress())
 }
